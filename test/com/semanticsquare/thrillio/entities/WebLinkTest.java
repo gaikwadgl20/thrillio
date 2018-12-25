2a59c3c6301573951cbb39a03b3b496753cc054b
Ganesh Gaikwad <gaikwadgl20@gmail.com>
2018-12-25 23:03:03 +0530

    JUnit Class

---
+++ test/com/semanticsquare/thrillio/entities/WebLinkTest.java
@@ -0,0 +1,14 @@
+package com.semanticsquare.thrillio.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WebLinkTest {
+
+	@Test
+	void testIsKidFriendly() {
+		fail("Not yet implemented");
+	}
+
+}
